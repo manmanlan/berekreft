52020e98cbdd50cd2830754daf07f76ebd07d1fb
manmanlan <prikitiki860@gmail.com>
2025-03-31 11:18:28 +0200

    aaaaaa

--- plast_spawner.gd
+++ plast_spawner.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 const PLAST = preload("res://leo/plast.tscn")
 
+var increasing_speed=0
+
+
 func _on_timer_timeout() -> void:
 	var plast_temp=PLAST.instantiate()
 	add_child(plast_temp)
+	plast_temp.speed+=increasing_speed
+	increasing_speed+=0.2
 	var ran = randi_range(1, 3)  # Corrected to use only two parameters
 
 	# Using an integer value for comparison
